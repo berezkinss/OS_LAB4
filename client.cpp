c294f0a4394f4883da678e5353379788209e45d5
berezkinss <105050504+berezkinss@users.noreply.github.com>
2023-03-23 23:35:30 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,30 +1,104 @@
-#include <stdlib.h>
 #include <windows.h>
 #include <iostream>
-#include <string>
-
 using namespace std;
+void WINAPI Callback(DWORD dwErrorCode, DWORD dwNumberOfBytesTransfered, LPOVERLAPPED lpOverlapped) {
+	cout << "Информация получена." << endl;
+}
 
-void CALLBACK CompletionRoutine(DWORD errorCode, DWORD bytestransfered, LPOVERLAPPED lpOverlapped) { }
+int main()
+{
+	system("chcp 1251");
+	system("cls");
 
-int main() {
-	HANDLE pipe;
-	string name;
-	char* data = new char[512];
-	OVERLAPPED ovr;
-	BOOL work = true, get;
-	ZeroMemory(&ovr, sizeof(ovr));
-	name = "\\\\.\\pipe\\newpipe";
-	pipe = CreateFileA(name.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	printf("Server data: \n");
-	while (work) {
-		get = ReadFileEx(pipe, data, 512, &ovr, CompletionRoutine);
-		if (get && pipe != INVALID_HANDLE_VALUE) {
-			SleepEx(INFINITE, true);
-      printf("%s\n", data);
-		}
-		else work = false;
+	CHAR buffer[512];										
+	BOOL isConnected = FALSE;								
+	LPCTSTR Pipename = TEXT("\\\\.\\pipe\\berezkin");		
+
+	HANDLE Event = CreateEvent(NULL, FALSE, FALSE, NULL);	
+	HANDLE Pipe = CreateFile(Pipename,						
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_FLAG_OVERLAPPED,
+		NULL
+	);
+
+	int choose;												
+
+	OVERLAPPED overlapped = OVERLAPPED();					
+
+	
+
+	if (Event != INVALID_HANDLE_VALUE && Pipe != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			system("cls");
+
+			cout << "+---+---------------------------------------+" << endl;
+			cout << "| 1 | Принять сообщение                     |" << endl;
+			cout << "+---+---------------------------------------+" << endl;
+			cout << "| 2 | Отсоединиться от именованного канала  |" << endl;
+			cout << "+---+---------------------------------------+" << endl;
+			cout << "| 0 | Выйти                                 |" << endl;
+			cout << "+---+---------------------------------------+" << endl;
+
+			cin >> choose;
+
+			switch (choose) {
+
+			case 1:
+
+				overlapped.hEvent = Event;
+																									
+									
+				isConnected = ReadFileEx(Pipe, (LPVOID)buffer, 512, &overlapped, Callback);	
+				if (isConnected)
+				{
+					cout << buffer << endl;
+					system("pause");
+				}
+				else
+				{
+					cout << "Чтение не удалось!" << endl;
+					system("pause");
+				}
+
+
+
+
+				break;
+
+			case 2:
+
+				isConnected = CloseHandle(Pipe);	
+
+				if (isConnected)
+					cout << "Вы были отсоединены от именованного канала!" << endl;
+				else
+					cout << "Не удалось отсоединиться!" << endl;
+
+				isConnected = FALSE;
+
+				system("pause");
+
+				break;
+
+			case 0:
+
+				break;
+
+			default:
+
+				break;
+
+			}
+
+		} while (choose);
 	}
+	else
+		cout << "Не удалось создать именованный канал, перезапустите программу!" << endl;
 
 	return 0;
 }
